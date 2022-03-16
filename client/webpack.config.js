d0b83d39a72d6fc0e9af282d895b728ebdb60c2b
John Climie <johnclimie00@gmail.com>
2022-03-16 13:35:39 -0400

    Added CSS Loader and babel

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,5 +1,5 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const WebpackPwaManifest = require('webpack-pwa-manifest');
+const WebpackPwaManifest = require('webpack-pwa -manifest');
 const path = require('path');
 const { InjectManifest } = require('workbox-webpack-plugin');
 
@@ -23,7 +23,21 @@ module.exports = () => {
 
     module: {
       rules: [
-        
+        {
+          test: /\.css$/i,
+          use: ['style-loader', 'css-loader'],
+        },
+        {
+          test: /\.m?js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env'],
+              plugins: ['@babel/plugin-proposal-object-rest-spread', '@babel/transform-runtime'],
+            },
+          },
+        },
       ],
     },
   };
