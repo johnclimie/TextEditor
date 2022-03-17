88c2823547cc22aa69daf734cd86b5a053f6ddae
John Climie <johnclimie00@gmail.com>
2022-03-17 11:33:27 -0400

    Created funcitonal manifest app

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,5 +1,5 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const WebpackPwaManifest = require('webpack-pwa -manifest');
+const WebpackPwaManifest = require('webpack-pwa-manifest');
 const path = require('path');
 const { InjectManifest } = require('workbox-webpack-plugin');
 
